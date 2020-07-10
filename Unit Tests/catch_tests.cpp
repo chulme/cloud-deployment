0505f2366e9dcfbd5cb66fc58f9d25b2884a1236
Chris Hulme <chris_hulme99@outlook.com>
2020-07-10 15:44:59 +0100

    Fixed no test results appearing

--- Unit Tests/catch_tests.cpp
+++ Unit Tests/catch_tests.cpp
@@ -45,8 +45,8 @@ TEST_CASE("Incrementing some numbers inside sections")
 int main()
 {
     Catch::Session session;
-    char*args[5]= {(char*) "Hello World", (char*) "-r", (char*) "junit",
-        (char*) "--out", (char*) "test-results.xml"};
+    char*args[6]= {(char*) "Hello World", (char*) "-r", (char*) "junit",
+        (char*) "--out", (char*) "test-results.xml", (char*)"--success"};
     
     int result = session.run(6,args);
     
